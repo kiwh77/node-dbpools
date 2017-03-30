4cd7d24efbf2c4087e0cbdcb10a09d8ce2cc8e34
wuhao <308801737@qq.com>
2017-03-30 11:01:55 +0800

    修复小问题

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+/** sybase数据库连接池 */
+module.exports.sybasePool = require('./src/sybase/Dbpool');
+/** sql处理 */
+module.exports.sybaseConditions = require('./src/sybase/DbConditions');
+/** 类似mongo模型*/
+module.exports.sybaseSchema = require('./src/sybase/DbSchema');
